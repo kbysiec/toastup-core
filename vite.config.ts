469e6c655d38d7cfc2e73760b9b2437fae40c86c
Kamil Bysiec <kamil.bysiec.agileplayers@gmail.com>
2024-01-23 14:55:35 +0100

    build: add alias for src and root paths

--- vite.config.ts
+++ vite.config.ts
@@ -35,5 +35,11 @@ module.exports = defineConfig({
   },
   test: {
 
+  },
+  resolve: {
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, "src") },
+      { find: "@@", replacement: path.resolve(__dirname) },
+    ]
   }
 });
