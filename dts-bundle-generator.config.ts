6b37480529ef9777c281fd8c9dc5912b2c01a79a
Kamil Bysiec <kamil.bysiec.agileplayers@gmail.com>
2024-01-31 09:37:06 +0100

    build: add csstype import in dts bundle config

--- dts-bundle-generator.config.ts
+++ dts-bundle-generator.config.ts
@@ -1,16 +1,12 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const packageJson = require("./package.json");
-
-const getPackageName = () => {
-  return packageJson.name;
-};
-
 const config = {
   entries: [
     {
       filePath: "./src/index.ts",
-      outFile: `./dist/${getPackageName()}.d.ts`,
+      outFile: `./dist/index.d.ts`,
       noCheck: false,
+      libraries: {
+        importedLibraries: ["csstype"],
+      },
     },
   ],
 };
