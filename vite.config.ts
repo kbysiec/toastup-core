bd65c7577c62a6da384fb504475ae8fe96375848
Kamil Bysiec <kamil.bysiec.agileplayers@gmail.com>
2024-04-21 20:03:09 +0200

    build: remove redundant code from build config

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,7 @@ import { defineConfig } from "vite";
 import packageJson from "./package.json";
 
 const getPackageName = () => {
-  return packageJson.name;
-};
-
-const getPackageNameCamelCase = () => {
-  try {
-    return getPackageName().replace(/-./g, char => char[1].toUpperCase());
-  } catch (err) {
-    throw new Error("Name property in package.json is missing.");
-  }
+  return packageJson.name.replace(/@/g, "").replace(/\//g, "-");
 };
 
 const fileName = {
@@ -28,7 +20,6 @@ module.exports = defineConfig({
     outDir: "./dist",
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
-      name: getPackageNameCamelCase(),
       formats,
       fileName: format => fileName[format],
     },
